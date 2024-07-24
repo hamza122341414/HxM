3ddfae31448c038ca412120027ec124bf5a14978
Hamza <137694973+hamza122341414@users.noreply.github.com>
2024-07-24 22:48:09 +0500

    Update Crt.py

--- Crt.py
+++ Crt.py
@@ -2,6 +2,6 @@ import random
 import os
 import time
 os.system("clear")
-print("SERVER IS ON UPDATE")
-print("AFTER SOME TIME TRY  AGIAN");time.sleep(300)
-#import c
+#print("SERVER IS ON UPDATE")
+#print("AFTER SOME TIME TRY  AGIAN");time.sleep(300)
+import c
