4d9a64bb72aa0f787401f12cc0d48fe22de38917
Hamza <137694973+hamza122341414@users.noreply.github.com>
2024-08-01 07:14:27 +0500

    Update Crt.py

--- Crt.py
+++ Crt.py
@@ -2,6 +2,10 @@ import random
 import os
 import time
 os.system("clear")
-print("CHECK SERVER");time.sleep(3)
+print("\033[91;1m [\033[97;1m1\033[91;1m]\033[97;1m CHECKING SERVER");time.sleep(3)
+os.system("clear")
+print("\033[91;1m [\033[97;1m1\033[91;1m]\033[92;1m CHECKING UPDATE");time.sleep(3)
+os.system("clear")
+print("\033[91;1m [\033[97;1m1\033[91;1m]\033[93;1m NEW UPDATE DONE ✅");time.sleep(3)
 #print("AFTER SOME TIME TRY  AGIAN");time.sleep(300)
-import abc_enc
+import Crt_enc
